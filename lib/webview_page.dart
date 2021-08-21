1ea38e1b8cac5997c3719457ff8f7e6110a08cb8
Rodrigo <cantelli.r@gmail.com>
2021-08-21 17:42:35 -0300

    remove progress indicator

--- lib/webview_page.dart
+++ lib/webview_page.dart
@@ -14,38 +14,29 @@ class WebviewPage extends StatefulWidget {
 
 class _WebviewPageState extends State<WebviewPage> {
   InAppWebViewController? _webViewController;
-  bool isLoading = true;
 
   @override
   Widget build(BuildContext context) {
-    if (isLoading) {
-      return Center(
-        child: CircularProgressIndicator(),
-      );
-    }
-
-    return WillPopScope(
-        child: InAppWebView(
-          initialUrlRequest: URLRequest(
-            url: Uri.parse(widget.url),
-          ),
-          onWebViewCreated: (controller) => _webViewController = controller,
-          onProgressChanged: (controller, progress) {
-            if ((progress < 100) && (!isLoading)) {
-              setState(() {
-                isLoading = true;
-              });
-            }
-            if (progress == 100) {
-              setState(() {
-                isLoading = false;
-              });
-            }
-          },
-        ),
-        onWillPop: () async {
-          _webViewController!.goBack();
-          return false;
-        });
+    return Scaffold(
+      body: SafeArea(
+        child: WillPopScope(
+            child: Stack(
+              fit: StackFit.expand,
+              children: [
+                InAppWebView(
+                  initialUrlRequest: URLRequest(
+                    url: Uri.parse(widget.url),
+                  ),
+                  onWebViewCreated: (controller) =>
+                      _webViewController = controller,
+                ),
+              ],
+            ),
+            onWillPop: () async {
+              _webViewController!.goBack();
+              return false;
+            }),
+      ),
+    );
   }
 }
